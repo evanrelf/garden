c584da2590f5252a8ed93292574d49812b2839ed
Evan Relf <evan@evanrelf.com>
2026-03-25 13:25:00 -0700

    Use dendritic pattern

--- flake.nix
+++ flake.nix
@@ -4,11 +4,12 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+    import-tree.url = "github:vic/import-tree/a037ed2"; # v0.1.0
     nixpkgs.url = "github:NixOS/nixpkgs";
   };
 
   outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ];
-    };
+    inputs.flake-parts.lib.mkFlake
+      { inherit inputs; }
+      (inputs.import-tree ./modules);
 }
---
+++ modules/systems.nix
@@ -0,0 +1,5 @@
+{ ... }:
+
+{
+  systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ];
+}
