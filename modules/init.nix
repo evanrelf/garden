349aebc43dcedebcff454ad6d50685db94ee11b1
Evan Relf <evan@evanrelf.com>
2026-03-25 14:59:21 -0700

    Overlay flake's `packages` into Nixpkgs

--- modules/init.nix
+++ modules/init.nix
@@ -1,13 +1,18 @@
-{ inputs, ... }:
+{ config, inputs, lib, ... }:
 
 {
   systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ];
+
+  flake.overlays.default = lib.composeManyExtensions [
+    (final: prev: config.flake.packages.${prev.stdenv.hostPlatform.system})
+  ];
 
   perSystem = { pkgs, system, ... }: {
     _module.args.pkgs =
       import inputs.nixpkgs {
         inherit system;
         config = { allowUnfree = true; };
+        overlays = [ config.flake.overlays.default ];
       };
 
     legacyPackages = pkgs;
